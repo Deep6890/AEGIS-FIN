05162c1742f9b34c9ee5f97455b844125c26852c
Deep6890 <deepkayastha6890@gmail.com>
2026-03-15 23:59:35 +0530

    clean: clear components and pages, reset App and CSS

--- stack/frontend/src/App.jsx
+++ stack/frontend/src/App.jsx
@@ -1,53 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import RiskAnalysis from "./pages/RiskAnalysis";
-import NotFound from "./pages/NotFound";
-import MainNavBar from "./components/Navbar/MainNavBar";
 
 function App() {
   return (
     <Router>
-      <AppContent />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
-  );
-}
-
-function AppContent() {
-  const navigate = useNavigate();
-
-  return (
-    <div className="min-h-screen bg-[#000000] text-white">
-
-      {/* HEADER */}
-      <header className="">
-        <div className="max-w-7xl mx-auto px-6 py-3 flex items-center justify-between">
-
-          {/* Logo */}
-          <h1
-            className="text-xl sm:text-2xl font-semibold tracking-wide cursor-pointer"
-            onClick={() => navigate('/')}
-          >
-            Smart<span className="text-emerald-400">Flare</span>
-          </h1>
-
-          {/* Navbar */}
-          <div className="hidden md:flex flex-1 justify-center">
-            <MainNavBar />
-          </div>
-
-        </div>
-      </header>
-
-      {/* PAGE CONTENT */}
-      <main className="max-w-full mx-auto px-6 py-8">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/risk-analysis" element={<RiskAnalysis />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-
-    </div>
   );
 }
 
---
+++ stack/frontend/src/components/Navbar/HeaderNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function HeaderNav() {
+    return (
+        <>
+            <div className='w-full h-[90px] bg-gray-200 justify-between flex'>
+                <div className='w-3/5 h-auto bg-black flex flex-col items-center p-1 justify-center gap-1'>
+                    <h1 className='text-white font-bold w-full'>AEGIS FIN</h1>
+                    <h2 className='text-white font-light w-full'> HI  </h2>
+                </div>
+            </div>
+        </>
+    )
+}
